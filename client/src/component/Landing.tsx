937d1bfc5f087dfe4d79864c18450c6f2be3cf20
runningpoem30 <aryaanandpathak30@gmail.com>
2025-08-06 00:52:35 +0530

    changes

---
+++ client/src/component/Landing.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function Landing() {
+    const [name , setName] = useState(0);
+    
+  return (
+    <div>
+      this is the landing page nigga
+    </div>
+  )
+}
+
+export default Landing
